e02dc126386df40c38956744ab333d441ab606bc
743834110 <743834110@qq.com>
2018-09-03 18:12:01 +0800

    react learning complete

---
+++ h2demo-web/src/main/webapp/importRequire.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports={
+    add: add
+}
+
+add = (a, b) => {
+    return a + b;
+}
--- h2demo-web/src/main/webapp/reactdemo.js
+++ h2demo-web/src/main/webapp/reactdemo.js
@@ -1,3 +1,5 @@
+
+
 // const element = <h1>Hello, world! </h1>
 // ReactDOM.render(
 //     element,
@@ -306,6 +308,7 @@ class Toggle extends React.Component {
         console.log(self.preventDefault())
     };
 
+    // 属性初始化器语法
     handleClick2 = () => {
         console.log(this + ":" + (this.i + this.j))
     };
@@ -313,9 +316,10 @@ class Toggle extends React.Component {
     handleClick(e) {
         console.log(e)
         this.handleClick2();
-        this.setState(preState => ({
-            toggleOn: !preState.toggleOn
-        }))
+        this.setState(preState => {
+
+            return {toggleOn: !preState.toggleOn}
+        })
     }
 
     render() {
@@ -333,3 +337,462 @@ ReactDOM.render(
     <Toggle/>,
     document.getElementById("example9")
 )
+
+// 事件绑定
+function ActionLink() {
+    function handleClick(params, e) {
+        console.log(params);
+    }
+
+    return (
+        <a href="#" onClick={handleClick.bind(this, 'fdfd')}>
+            点我
+        </a>
+    );
+}
+
+$(function () {
+    ReactDOM.render(
+        <ActionLink/>,
+        document.getElementById("example10")
+    )
+})
+
+// 条件渲染
+class UserGreeting extends React.Component {
+
+    render() {
+        return (
+            <h3>
+                欢迎回来
+            </h3>
+        )
+    }
+}
+
+class GuestGreeting extends React.Component {
+
+    render() {
+        return (
+            <h3>
+                请先注册
+            </h3>
+        )
+    }
+}
+
+function Greeting(props) {
+
+    if (props.login){
+        return <UserGreeting/>
+    }
+    else
+        return (
+            <GuestGreeting/>
+        )
+}
+
+function LoginButton(props) {
+
+    return (
+        <button onClick= {props.onClick}>
+            登录
+        </button>
+    )
+}
+function LogoutButton(props) {
+
+    return (
+        <button onClick = {props.onClick}>
+            退出
+        </button>
+    )
+}
+
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            login: false
+        }
+    }
+    // 相应按钮点击事件
+    handleClick = () => {
+        this.setState( preState => ({
+            login: !preState.login
+        }))
+    }
+
+    render() {
+
+        const login = this.state.login;
+        let button;
+        if (login) {
+            button = <LoginButton onClick = {this.handleClick}/>
+        }
+        else {
+            button = <LogoutButton onClick = {this.handleClick}/>
+        }
+
+
+        return (
+            <div>
+                <Greeting login = {login}/>
+                {button}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <LoginControl/>,
+    document.getElementById("example11")
+)
+
+
+function Mailbox(props) {
+
+    const unReadMessage = props.unReadMessages;
+    console.log(unReadMessage.length > 0)
+    return (
+        <div>
+            <h1>Hello</h1>
+            {
+                unReadMessage.length > 0? (
+                    <h2>
+                        你有 {unReadMessage.length }条消息信息
+                    </h2>
+                ): (
+                    <h2>尚没有信息需要查看</h2>
+                )
+            }
+        </div>
+    )
+
+
+}
+
+const messages = []
+ReactDOM.render(
+    <Mailbox unReadMessages = {messages}/>,
+    document.getElementById("example12")
+)
+
+// React列表&Keys
+function ListItem(props) {
+    return (
+        <li>
+            {props.value}
+        </li>
+    )
+}
+
+function NumberList(props) {
+    const listItems = props.numbers.map(number => {
+
+        return <ListItem
+            key={number.toString()}
+            value={number}/>
+        }
+
+    );
+    return (
+        <ul>
+            {listItems}
+        </ul>
+    )
+}
+const numbers = [1,2,3,4,5,6,7,8,9]
+ReactDOM.render(
+    <NumberList numbers={numbers}/>,
+    document.getElementById("example13")
+)
+
+// React组件API
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: 0};
+        this.setNewNumber = this.setNewNumber.bind(this);
+    }
+
+    setNewNumber() {
+        this.setState({data: this.state.data + 1})
+    }
+    render() {
+        return (
+            <div>
+                <button onClick = {this.setNewNumber}>INCREMENT</button>
+                <Content myNumber = {this.state.data}/>
+            </div>
+        );
+    }
+}
+
+/**
+ * 生命周期函数运行顺序：
+ * 如下方法的书写顺序
+ */
+class Content extends React.Component {
+
+    componentWillMount() {
+        console.log('Component WILL MOUNT!')
+    }
+    componentDidMount() {
+        console.log('Component DID MOUNT!')
+    }
+    componentWillReceiveProps(newProps) {
+        console.log('Component WILL RECEIVE PROPS!')
+    }
+    shouldComponentUpdate(newProps, newState) {
+        return true;
+    }
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Component WILL UPDATE!');
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Component DID UPDATE!')
+    }
+    componentWillUnmount() {
+        console.log('Component WILL UNMOUNT!')
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.props.myNumber}</h3>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <div>
+        <Button />
+    </div>,
+    document.getElementById('example14')
+);
+
+// React AJAX
+class Component extends React.Component{}
+
+class UserGist extends Component {
+
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {username: '', lastGistUrl: ''};
+    // }
+
+    componentWillMount() {
+        this.state = {username: '', lastGistUrl: ''};
+    }
+
+    // 组件挂载完成后进行调用
+    componentDidMount() {
+        this.serverRequest = $.get(this.props.source, function (result) {
+            var lastGist = result[0];
+            this.setState({
+                username: lastGist.owner.login,
+                lastGistUrl: lastGist.html_url
+            })
+
+        }.bind(this));
+    }
+
+    componentWillUnmount() {
+        this.serverRequest.abort();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.username} 用户最新的Gist共享地址为：
+                <a href = {this.state.lastGistUrl}>{this.state.lastGistUrl}</a>
+            </div>
+        )
+    }
+
+}
+
+ReactDOM.render(
+    <UserGist source = "https://api.github.com/users/octocat/gists"/>,
+    document.getElementById("example14")
+)
+
+// input输入框
+class HelloMessage1 extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: "Hello Runoob!"
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    render() {
+        let value = this.state.value;
+
+        return (
+            <div>
+                <input type="text" value={value} onChange={this.handleChange}/>
+                <h4>{value}</h4>
+            </div>
+        )
+    }
+}
+ReactDOM.render(
+    <HelloMessage1 />,
+    document.getElementById("example15")
+)
+
+// select下拉菜单
+class FalvorForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "请选择你最喜欢的网站",
+            sites:[]
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        })
+    };
+
+    handleSubmit = (event) => {
+        alert("Your favorite flavor is " + this.state.value)
+    };
+
+
+    componentDidMount() {
+        this.setState({
+            sites: [
+                {
+                    id: 'gg',
+                    name: 'Google'
+                },
+                {
+                    id: 'rn',
+                    name: "Runoob"
+                },
+                {
+                    id: "tb",
+                    name: "Taobao"
+                },
+                {
+                    id: "fb",
+                    name: "Facebood"
+                }
+            ]
+        })
+    }
+
+    render() {
+
+        return (
+            <form>
+                <label>
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        {
+                            this.state.sites.map(site =>
+                                <option key={site.id} value={site.id}>{site.name}</option>
+                            )
+                        }
+                    </select>
+                </label>
+            </form>
+        )
+    }
+}
+ReactDOM.render(
+    <FalvorForm/>,
+    document.getElementById("example16")
+)
+
+// 多个表单
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        // 让函数根据event.target.name的值来选择做什么
+        this.setState({
+            [name]: value
+        });
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    是否离开:
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    访客数:
+                    <input
+                        name="numberOfGuests"
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Reservation/>,
+    document.getElementById("example17")
+)
+
+// refs: 获取某组件的引用
+class MyComponent extends React.Component {
+
+    handleClick() {
+        // 使用原生的DOM的API获取焦点
+        var myInput = this.refs.myInput
+        console.log($(myInput))
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" ref="myInput"/>
+                <input type="button" value="点我输入框获取焦点" onClick={this.handleClick.bind(this)}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <MyComponent/>,
+    document.getElementById("example18")
+)
+
